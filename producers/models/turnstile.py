1dec413c9298142624887d29135ef2e5efb15fcd
jkoth <kothari.jubin88@gmail.com>
2020-09-01 23:54:23 -0500

    Add config parameters for Turnstile Producer

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -13,13 +13,7 @@ logger = logging.getLogger(__name__)
 
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_value.json")
 
     def __init__(self, station):
         """Create the Turnstile"""
@@ -31,18 +25,14 @@ class Turnstile(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+        topic_name = f"{super().NAMESPACE}.turnstile"
+
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            topic_name=topic_name,
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_partitions=3,
+            num_replicas=3,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
