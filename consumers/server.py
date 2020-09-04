a0eaceab9eb354249ef4af3e87bb4069e1cb3111
jkoth <kothari.jubin88@gmail.com>
2020-09-03 23:34:06 -0500

    Update Kafka Topic names for Consumers

--- consumers/server.py
+++ consumers/server.py
@@ -41,12 +41,12 @@ class MainHandler(tornado.web.RequestHandler):
 
 def run_server():
     """Runs the Tornado Server and begins Kafka consumption"""
-    if topic_check.topic_exists("TURNSTILE_SUMMARY") is False:
+    if topic_check.topic_exists("cta.trains.monitor.turnstile.summarized") is False:
         logger.fatal(
             "Ensure that the KSQL Command has run successfully before running the web server!"
         )
         exit(1)
-    if topic_check.topic_exists("org.chicago.cta.stations.table.v1") is False:
+    if topic_check.topic_exists("cta.trains.monitor.stations.transformed") is False:
         logger.fatal(
             "Ensure that Faust Streaming is running successfully before running the web server!"
         )
@@ -63,23 +63,23 @@ def run_server():
     # Build kafka consumers
     consumers = [
         KafkaConsumer(
-            "org.chicago.cta.weather.v1",
+            "cta.trains.monitor.weather",
             weather_model.process_message,
             offset_earliest=True,
         ),
         KafkaConsumer(
-            "org.chicago.cta.stations.table.v1",
+            "cta.trains.monitor.stations.transformed",
             lines.process_message,
             offset_earliest=True,
             is_avro=False,
         ),
         KafkaConsumer(
-            "^org.chicago.cta.station.arrivals.",
+            "^cta.trains.monitor.stations.arrivals.*",
             lines.process_message,
             offset_earliest=True,
         ),
         KafkaConsumer(
-            "TURNSTILE_SUMMARY",
+            "cta.trains.monitor.turnstile.summarized",
             lines.process_message,
             offset_earliest=True,
             is_avro=False,
